aed59a4ef9a6b0130767f1f5073a777fb42734b2
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2024-02-16 23:07:15 +0100

    levenshtein_distance implemented

---
+++ YuriAtlas/YuriMangaListAccess/LevensteinsDistance.py
@@ -0,0 +1,39 @@
+def levenshtein_distance(str1, str2):
+    if str1 == str2:
+        return 0
+
+    row_count = len(str1) + 1
+    col_count = len(str2) + 1
+
+    matrix = [[row + col if row == 0 or col == 0 else None for col in range(col_count)] for row in range(row_count)]
+
+    for i in range(row_count):
+        matrix[i][0] = i
+
+    for j in range(1, col_count):
+        matrix[0][j] = j
+
+    for i in range(1, row_count):
+        for j in range(1, col_count):
+
+            insertion = matrix[i - 1][j] + 1
+            deletion = matrix[i][j - 1] + 1
+
+            substitution_cost = matrix[i - 1][j - 1]
+
+            if str1[i - 1] != str2[j - 1]:
+                substitution_cost += 1
+
+            min_cost = min(insertion, deletion, substitution_cost)
+            matrix[i][j] = min_cost
+
+    return matrix[row_count - 1][col_count - 1]
+
+
+if __name__ == '__main__':
+
+    s1 = 'book'
+    s2 = 'back'
+
+    result = levenshtein_distance(s1, s2)
+    print(f'The Levenshtein distance between "{s1}" and "{s2}" is {result}')
