e36d9bf25f34d71a00ab0833e63dff943e979a05
Llyn Baumann <darkizockt16@gmail.com>
2024-03-15 13:24:29 +0100

    write result to json file

--- YuriAtlas/YuriMangaProcessing/Preprocessing/Genres/genrated_genre_list.py
+++ YuriAtlas/YuriMangaProcessing/Preprocessing/Genres/genrated_genre_list.py
@@ -1,16 +1,24 @@
 from YuriMangaListAccess.SpreadSheetAccess import get_unfiltered_genres
 from collections import Counter
+import json
 
 
 def get_genre_list():
     unfiltered_genres = get_unfiltered_genres()
 
     genres_count = Counter(unfiltered_genres)
-    genre_list = genres_count.most_common(20)
+    genres = genres_count.most_common(20)
 
-    genre_list = list(filter(lambda g: g[1] > 10, genre_list))
+    genres = list(filter(lambda g: g[1] > 10, genres))
 
-    print(genre_list)
+    genre_names: list[str] = [genre for genre, count in genres]
+
+    genre_list = [(genre, index) for index, genre in enumerate(genre_names)]
+
+    with open('./genre-list.json', 'w') as outfile:
+        json.dump(genre_list, outfile, indent="genres")
+
+    print('Saved to genre-list.json')
 
 
 if __name__ == "__main__":
