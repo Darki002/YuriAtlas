dd288001d5ce48fe76d72ae5cd546f41b3849a26
Llyn Baumann <darkizockt16@gmail.com>
2024-03-15 13:48:27 +0100

    Update genrated_genre_list.py

--- YuriAtlas/YuriMangaProcessing/Preprocessing/Genres/genrated_genre_list.py
+++ YuriAtlas/YuriMangaProcessing/Preprocessing/Genres/genrated_genre_list.py
@@ -2,14 +2,17 @@ from YuriMangaListAccess.SpreadSheetAccess import get_unfiltered_genres
 from collections import Counter
 import json
 
+MAX_GENRES = 20
+MIN_USAGES = 20
+
 
 def get_genre_list():
     unfiltered_genres = get_unfiltered_genres()
 
     genres_count = Counter(unfiltered_genres)
-    genres = genres_count.most_common(20)
+    genres = genres_count.most_common(MAX_GENRES)
 
-    genres = list(filter(lambda g: g[1] > 10, genres))
+    genres = list(filter(lambda g: g[1] > MIN_USAGES, genres))
 
     genre_names: list[str] = [genre for genre, count in genres]
 
