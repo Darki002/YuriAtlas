70c425d3fb6abce3c279e8ac83949c2c26609f69
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2024-03-17 17:45:22 +0100

    make recomendation system ready

--- YuriAtlas/yuri_manga_processing/recommendation_system/main.py
+++ YuriAtlas/yuri_manga_processing/recommendation_system/main.py
@@ -1,2 +1,17 @@
-def start():
-    print("Welcome to Yuri Manga")
+from yuri_manga_processing.yuri_manga import YuriManga
+
+MIN_SCORE = 6
+COMPLETED_INDEX = 1
+PLAN_TO_READ_INDEX = 4
+
+
+def create_recommendation(user_reading_list: list[YuriManga], spreadsheet: list[YuriManga]) -> list[YuriManga]:
+
+    completed_mangas = [manga for manga in user_reading_list if manga.get_user_reading_status() == COMPLETED_INDEX]
+    plan_to_read = [manga for manga in user_reading_list if manga.get_user_reading_status() == PLAN_TO_READ_INDEX]
+
+    completed_favorites = [manga for manga in completed_mangas if manga.user_score >= MIN_SCORE]
+
+    # ka
+
+    return []
