1ef21958b6c57944ee6753355ab2afa53916916c
Llyn Baumann <113591988+Darki002@users.noreply.github.com>
2024-03-17 20:15:23 +0100

    Update main.py

--- YuriAtlas/yuri_manga_processing/recommendation_system/main.py
+++ YuriAtlas/yuri_manga_processing/recommendation_system/main.py
@@ -1,5 +1,5 @@
 from yuri_manga_processing.yuri_manga import YuriManga
-from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 MIN_SCORE = 6
 COMPLETED_INDEX = 1
@@ -7,12 +7,9 @@ PLAN_TO_READ_INDEX = 4
 
 
 def create_recommendation(user_reading_list: list[YuriManga], spreadsheet: list[YuriManga]) -> list[YuriManga]:
-
     completed_mangas = [manga for manga in user_reading_list if manga.get_user_reading_status() == COMPLETED_INDEX]
     plan_to_read = [manga for manga in user_reading_list if manga.get_user_reading_status() == PLAN_TO_READ_INDEX]
 
     completed_favorites = [manga for manga in completed_mangas if manga.user_score >= MIN_SCORE]
-
-    # ka
 
     return []
