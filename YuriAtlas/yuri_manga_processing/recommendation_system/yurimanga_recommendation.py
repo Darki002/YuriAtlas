887e26228b71cfdf5616c96f1b3ffebae7864e00
Llyn Baumann <darkizockt16@gmail.com>
2024-03-22 08:21:39 +0100

    property for easy acces in YuriMangaRecommendation

--- YuriAtlas/yuri_manga_processing/recommendation_system/yurimanga_recommendation.py
+++ YuriAtlas/yuri_manga_processing/recommendation_system/yurimanga_recommendation.py
@@ -6,3 +6,28 @@ class YuriMangaRecommendation:
         self.manga_id: int = manga_id
         self.manga: YuriManga = manga
         self.transformed_description = transformed_description
+
+    @property
+    def genres(self) -> list[int]:
+        return self.manga.get_genres()
+
+    @property
+    def nsfw_level(self) -> int:
+        return self.manga.get_nsfw_level()
+
+    @property
+    def manga_format(self) -> int:
+        return self.manga.get_manga_format()
+
+    @property
+    def publication(self) -> int:
+        return self.manga.get_publication()
+
+    @property
+    def user_reading_status(self) -> int:
+        return self.manga.get_user_reading_status()
+
+    @property
+    def user_score(self) -> int:
+        return self.manga.get_user_score()
+
