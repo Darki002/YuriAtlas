51116ca545cfc89e2b661a5c436b6d6caf8ab2f3
Darki002 <darkizockt16@gmail.com>
2024-04-12 12:51:49 +0200

    does this work?

--- YuriAtlas/yuri_manga_processing/preprocessing/genres/embedding/learn_embedding.py
+++ YuriAtlas/yuri_manga_processing/preprocessing/genres/embedding/learn_embedding.py
@@ -2,19 +2,38 @@ from data_access.yuri_manga_spreadsheet import spreadsheet_access
 from yuri_manga_processing.yuri_manga import YuriManga
 
 
-def learn(mangas: list[YuriManga], genres: list[str]):
+def learn(mangas_genres: list[list[str]], genres: list[str]) -> list[list[int]]:
     genre_count = len(genres)
     embedding = [[0 for _ in range(genre_count)] for _ in range(genre_count)]
     genres: dict[str, int] = {name: index for index, name in enumerate(genres)}
 
-    for i_manga in mangas:
-        for j_manga in mangas:
-            if j_manga == i_manga:
-                continue
+    for mg in mangas_genres:
+        for i_genre in mg:
+            i = genres[i_genre]
+            for j_genre in mg:
+                j = genres[j_genre]
+                if i == j:
+                    continue
+                embedding[i][j] += 1
+    return embedding
 
 
-if __name__ is '__main__':
-    print('Start...')
+def main():
     all_mangas = spreadsheet_access.get_all()
+    if all_mangas is None:
+        print("oops")
+        return
+
     all_genres = spreadsheet_access.get_all_genres()
-    learn(all_mangas, all_genres)
+    if all_genres is None:
+        print("oops")
+        return
+
+    result = learn(all_mangas, all_genres)
+    print(result)
+
+
+if __name__ == '__main__':
+    print('Start...')
+    main()
+
